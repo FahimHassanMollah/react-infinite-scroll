5de03acb5964b8907de1bcfa25d720820499c4bc
fahimhassanmollah <fahimhassan0197@gmail.com>
2024-02-27 15:58:51 +0600

    Update ProductCard component styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const ProductCard = ({product}) => {
   return (
-    <div className="card" style={{ maxWidth: '18rem' }}>
+    <div className="card mb-3" style={{ maxWidth: '18rem' }}>
     <img src={product?.thumbnail} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">{product?.title}</h5>
